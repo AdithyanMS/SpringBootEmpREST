7db2187308349ffa909ae63d045b2a54729c1b68
AdithyanMS <54012342+AdithyanMS@users.noreply.github.com>
2021-04-16 23:39:55 +0530

    Add files via upload

---
+++ SpringBootEmployeeREST/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	//@GeneratedValue
+	private int id;
+	private String name;
+	private String lang;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLang() {
+		return lang;
+	}
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", lang=" + lang + "]";
+	}
+	
+
+}
---
+++ SpringBootEmployeeREST/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into employee values(3,'Adi','Python');
+insert into employee values(43,'Niharuuu','CPP');
+insert into employee values(6,'kuttan','Python');
+insert into employee values(52,'Rahul','C');
+insert into employee values(19,'Fyabi','C');
